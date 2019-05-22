1860852284ec67db968bc05f6bc6d76c794561ee
Stig Atle Freitag <stig.freitag@knowit.no>
2019-05-22 14:37:20 +0200

    added api call to games

--- src/frontend/src/App.js
+++ src/frontend/src/App.js
@@ -2,21 +2,11 @@ import React from 'react';
 import './App.css';
 import GameList from './GameList'
 
-function getGameList() {
-  return [
-    {
-      id: 1,
-      date: "17.01.2019",
-      numberOfPlayers: 4,
-      winner: "Stig"
-    }
-  ]
-}
 function App() {
   return (
     <div className="App">
       <button>Start new game</button>
-      <GameList games={getGameList()}/>
+      <GameList />
     </div>
   );
 }
--- src/frontend/src/GameList.js
+++ src/frontend/src/GameList.js
@@ -1,34 +1,55 @@
 import React from 'react'
+import API from './api'
 
-function GameList(props) {
-  return (
-    <div>
-      <h2>Historical games</h2>
-      <table>
-        <thead>
-          <tr>
-            <th>Game</th>
-            <th>NumberOfPlayers</th>
-            <th>Date</th>
-            <th>Winner</th>
-          </tr>
-        </thead>
-        <tbody>
-          {props.games.map(game => {
-            return (
-              <tr>
-                <td>{game.id}</td>
-                <td>{game.numberOfPlayers}</td>
-                <td>{game.date}</td>
-                <td>{game.winner}</td>
-              </tr>
-            )
-          })}
-        </tbody>
-      </table>
-    </div>
+export default class GameList extends React.Component {
+  state = {
+    games: []
+  }
 
-  )
+  componentDidMount() {
+    API.get("game/all")
+    .then(res => {
+      const games = res.data;
+      this.setState({ games });
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }
+  render() {
+    return (
+      <div>
+        <h2>Historical games</h2>
+        {this.state.games.length > 0 ? (
+          <table>
+          <thead>
+            <tr>
+              <th>Game</th>
+              <th>NumberOfPlayers</th>
+              <th>Date</th>
+              <th>Winner</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.games.map(game => {
+              return (
+                <tr>
+                  <td>{game.id}</td>
+                  <td>{game.numberOfPlayers}</td>
+                  <td>{game.date}</td>
+                  <td>{game.winner}</td>
+                </tr>
+              )
+            })}
+          </tbody>
+        </table>
+        ) : (
+          <div>No historic games to show</div>
+        )}
+        
+      </div>
+  
+    )
+  }
+  
 }
-
-export default GameList;
